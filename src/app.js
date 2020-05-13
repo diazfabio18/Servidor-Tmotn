5ba4a6f923b0b40204faf14348de051be029609f
diazfabio18 <fabiodiaz@est.frba.utn.edu.ar>
2020-05-13 09:28:40 -0300

    cyatro

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(cors()); /*evito problemas de cors */
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 
 //Configuracion de destino cuando multer obtiene una image
 const storage = multer.diskStorage({
