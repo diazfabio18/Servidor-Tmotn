3b117e216bd0efe26b44ef27f0159632a6950910
diazfabio18 <fabiodiaz@est.frba.utn.edu.ar>
2020-05-13 09:20:59 -0300

    tre

--- src/database.js
+++ src/database.js
@@ -9,9 +9,20 @@ mongoose.set('useFindAndModify', false);
 
 //Conexion al servidor de mongo atlas
 mongoose.connect(process.env.MONGODB_NOMBRE, {
-    useNewUrlParser: true,
+    useNewUrlParser: true, 
+    useCreateIndex: true,
     useUnifiedTopology: true
 })
     .then(db => console.log('Base de datos conectada a mongo Atlas'))
     .catch(err => console.error(err));
 
+    /*const options = {
+        useNewUrlParser: true, 
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    }
+    mongoose.connect(uri,options)
+    .then(
+        () => {console.log('conectando a mongoose xdd');},
+        err => {err}
+    );*/
